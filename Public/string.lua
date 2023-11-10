f950f544cc420084b469c869beb8090e50565fd1
CatchMoon <84398459+ComeCatchMoonLa@users.noreply.github.com>
2023-11-11 01:00:44 +0800

    Update string.lua

--- Public/string.lua
+++ Public/string.lua
@@ -11,4 +11,32 @@ _G.string.split = function(str, delimiter)
     return result
 end
 
+-- 数字转字符串, 可设置保留几位小数, 以及是否影藏小数位末尾的0
+_G.string.num2Str = function(num, precision, hideZero)
+    if precision == nil then precision = 2 end
+    if hideZero == nil then hideZero = false end
+
+    if hideZero and num == 0 then return "0" end
+
+    local str = ("%." .. precision .. 'f'):format(num)
+
+    if hideZero and str:find('.') then
+        local cnt = 0
+        for i = #str, 1, -1 do
+            if str:sub(i, i) == '0' then
+                cnt = cnt + 1
+            elseif str:sub(i, i) == '.' then
+                cnt = cnt + 1
+                break
+            else
+                break
+            end
+        end
+
+        str = str:sub(1, #str - cnt)
+    end
+
+    return str
+end
+
 return {}
